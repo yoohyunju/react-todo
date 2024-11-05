b2f4e2a66f942c239200bc0a85fc676d24cb26c1
string_main <yhj2756@naver.com>
2024-11-06 02:27:56 +0900

    feat: 스타일 및 더미 데이터 추가

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 const ToDoList = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(["방청소", "파스타 만들기", "음악 틀기"]);
   const [inputVal, setInputVal] = useState("");
   const [editIdx, setEditIdx] = useState(null);
   const [editText, setEditText] = useState("");
@@ -33,35 +33,54 @@ const ToDoList = () => {
   };
 
   return (
-    <div className="todo-list">
-      <h1>ToDo List</h1>
-      <div>
-        <input
-          type="text"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-          placeholder="할 일을 입력해 주세요."
-        />
-        <button onClick={addTodo}>추가</button>
+    <div className="todo">
+      <h1 className="todo__title">ToDo List</h1>
+      <div className="todo__contents">
+        <div className="todo__input-group">
+          <input
+            type="text"
+            className="todo__input"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+            placeholder="할 일을 입력해 주세요."
+          />
+          <button className="todo__btn todo__btn--add" onClick={addTodo}>
+            ➕
+          </button>
+        </div>
+        <ul className="todo__list">
+          {todos.map((todo, idx) => (
+            <li key={idx} className="todo__item">
+              {editIdx === idx ? (
+                <input
+                  type="text"
+                  className="todo__input--edit"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                  onBlur={() => editTodo()}
+                />
+              ) : (
+                <span className="todo__text">{todo}</span>
+              )}
+
+              <div className="todo__btn-group">
+                <button
+                  className="todo__btn todo__btn--edit"
+                  onClick={() => startEditMode(idx)}
+                >
+                  ✏️
+                </button>
+                <button
+                  className="todo__btn todo__btn--delete"
+                  onClick={() => deleteTodo(idx)}
+                >
+                  ❌
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {todos.map((todo, idx) => (
-          <li key={idx}>
-            {editIdx === idx ? (
-              <input
-                type="text"
-                value={editText}
-                onChange={(e) => setEditText(e.target.value)}
-                onBlur={() => editTodo()}
-              />
-            ) : (
-              <span>{todo}</span>
-            )}
-            <button onClick={() => startEditMode(idx)}>수정</button>
-            <button onClick={() => deleteTodo(idx)}>삭제</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
